828a96cc9aaa8f4d09037e8c73c218b420a11adf
Justin Smith <justin.smith@summitsystemsinc.com>
2012-05-30 16:17:12 -0500

    added persistence for the tree.

--- modules/netbeans-opc-browser-plugin/src/main/java/com/summit/opc/nb/TreeBrowserTopComponent.java
+++ modules/netbeans-opc-browser-plugin/src/main/java/com/summit/opc/nb/TreeBrowserTopComponent.java
@@ -4,6 +4,9 @@
  */
 package com.summit.opc.nb;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import org.netbeans.api.settings.ConvertAsProperties;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
@@ -34,23 +37,32 @@ preferredID = "TreeBrowserTopComponent")
 })
 public final class TreeBrowserTopComponent extends TopComponent implements ExplorerManager.Provider {
 
+    public static final String CLS_ID_KEY = "clsId";
+    public static final String DOMAIN_KEY = "domain";
+    public static final String HOST_KEY = "host";
+    
+    public static final String PASSWORD_KEY = "password";
+    public static final String PROG_ID_KEY = "progId";
+    public static final String PROPERTY_VERSION = "1.0";
+    public static final String PROPERTY_VERSION_KEY = "version";
+    public static final String NUM_CONNECTION_KEY = "numConnections." + PROPERTY_VERSION_KEY;
+    public static final String USER_KEY = "user";
     private transient ExplorerManager explorerManager = new ExplorerManager();
     private ServersChildren serversChildren = new ServersChildren();
-
     public static TreeBrowserTopComponent INSTANCE = null;
-    
+
     public TreeBrowserTopComponent() {
         initComponents();
         setName(Bundle.CTL_TreeBrowserTopComponent());
         setToolTipText(Bundle.HINT_TreeBrowserTopComponent());
 
         associateLookup(ExplorerUtils.createLookup(explorerManager, getActionMap()));
-        explorerManager.setRootContext(new ServersNode(getServersChildren()));        
-        
+        explorerManager.setRootContext(new ServersNode(getServersChildren()));
+
         ConnectionInformation info = new ConnectionInformation();
         info.setDomain("");
-        
-        if(INSTANCE == null){
+
+        if (INSTANCE == null) {
             INSTANCE = this;
         }
     }
@@ -99,7 +111,6 @@ public final class TreeBrowserTopComponent extends TopComponent implements Explo
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         new AddOPCServerWizardAction().actionPerformed(evt);
     }//GEN-LAST:event_jButton1ActionPerformed
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JScrollPane jScrollPane1;
@@ -123,13 +134,38 @@ public final class TreeBrowserTopComponent extends TopComponent implements Explo
     void writeProperties(java.util.Properties p) {
         // better to version settings since initial version as advocated at
         // http://wiki.apidesign.org/wiki/PropertyFiles
-        p.setProperty("version", "1.0");
+        p.setProperty(PROPERTY_VERSION_KEY, PROPERTY_VERSION);
         // TODO store your settings
+        List<ConnectionInformation> connections = new ArrayList<ConnectionInformation>(getServersChildren().getConnections());
+        p.setProperty(NUM_CONNECTION_KEY, String.valueOf(connections.size()));
+        for (int i = 0; i < connections.size(); i++) {
+            p.setProperty(USER_KEY + "." + i + "." + PROPERTY_VERSION, connections.get(i).getUser());
+            p.setProperty(HOST_KEY + "." + i + "." + PROPERTY_VERSION, connections.get(i).getHost());
+            p.setProperty(CLS_ID_KEY + "." + i + "." + PROPERTY_VERSION, connections.get(i).getClsid());
+            p.setProperty(PASSWORD_KEY + "." + i + "." + PROPERTY_VERSION, connections.get(i).getPassword());
+            p.setProperty(PROG_ID_KEY + "." + i + "." + PROPERTY_VERSION, connections.get(i).getProgId());
+            p.setProperty(DOMAIN_KEY + "." + i + "." + PROPERTY_VERSION, connections.get(i).getDomain());
+        }
     }
 
     void readProperties(java.util.Properties p) {
         String version = p.getProperty("version");
         // TODO read your settings according to their version
+        if (version != null && version.equals(PROPERTY_VERSION)) {
+            List<ConnectionInformation> connections = new ArrayList<ConnectionInformation>();
+            int count = Integer.valueOf(p.getProperty(NUM_CONNECTION_KEY, "0"));
+            for(int i = 0; i < count; i++){
+                ConnectionInformation connInfo = new ConnectionInformation();
+                connInfo.setClsid(p.getProperty(CLS_ID_KEY+ "." + i + "." + PROPERTY_VERSION,"ERROR"));
+                connInfo.setProgId(p.getProperty(PROG_ID_KEY + "." + i + "." + PROPERTY_VERSION,"ERROR"));
+                connInfo.setUser(p.getProperty(USER_KEY + "." + i + "." + PROPERTY_VERSION,"ERROR"));
+                connInfo.setHost(p.getProperty(HOST_KEY + "." + i + "." + PROPERTY_VERSION,"ERROR"));
+                connInfo.setDomain(p.getProperty(DOMAIN_KEY + "." + i + "." + PROPERTY_VERSION,"ERROR"));
+                connInfo.setPassword(p.getProperty(PASSWORD_KEY + "." + i + "." + PROPERTY_VERSION,"ERROR"));
+                connections.add(connInfo);
+            }
+            serversChildren.setConnections(connections);
+        }
     }
 
     @Override
