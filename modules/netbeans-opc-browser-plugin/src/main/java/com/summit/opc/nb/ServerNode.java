9737e56dab53598a4c23b08ab4c427c2b09f4b55
Justin Smith <justin.smith@summitsystemsinc.com>
2012-06-27 17:31:35 -0500

    Added the server's properties to the propertysheet.

--- modules/netbeans-opc-browser-plugin/src/main/java/com/summit/opc/nb/ServerNode.java
+++ modules/netbeans-opc-browser-plugin/src/main/java/com/summit/opc/nb/ServerNode.java
@@ -6,10 +6,17 @@ package com.summit.opc.nb;
 
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.util.Collection;
 import org.openide.nodes.AbstractNode;
+import org.openide.nodes.PropertySupport;
+import org.openide.nodes.Sheet;
+import org.openide.util.Exceptions;
 import org.openide.util.ImageUtilities;
 import org.openide.util.lookup.Lookups;
 import org.openscada.opc.lib.common.ConnectionInformation;
+import org.openscada.opc.lib.da.Group;
+import org.openscada.opc.lib.da.browser.Branch;
+import org.openscada.opc.lib.da.browser.Leaf;
 
 /**
  *
@@ -39,5 +46,22 @@ public class ServerNode extends AbstractNode {
     @Override
     public Image getOpenedIcon(int type) {
         return getIcon(type);
+    }
+    
+    @Override
+    protected Sheet createSheet() {
+        Sheet sheet = Sheet.createDefault();
+        final Sheet.Set set = Sheet.createPropertiesSet();
+        try {
+            set.put(new PropertySupport.Reflection<String>(connInfo, String.class, "getDomain", null));
+            set.put(new PropertySupport.Reflection<String>(connInfo, String.class, "getHost", null));
+            set.put(new PropertySupport.Reflection<String>(connInfo, String.class, "getUser", null));
+            set.put(new PropertySupport.Reflection<String>(connInfo, String.class, "getClsid", null));
+            set.put(new PropertySupport.Reflection<String>(connInfo, String.class, "getProgid", null));
+        } catch (NoSuchMethodException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+        sheet.put(set);
+        return sheet;
     }
 }
